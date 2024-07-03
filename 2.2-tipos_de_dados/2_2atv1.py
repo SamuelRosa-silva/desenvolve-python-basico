618f10ca529b5e7d46989cb56dbd0c52287f6eba
SamuelRosa <163168836+SamuelRosa-silva@users.noreply.github.com>
2024-07-03 15:25:08 -0300

    Add files via upload

---
+++ 2.2-tipos_de_dados/2_2atv1.py
@@ -0,0 +1,4 @@
+num1 = 5
+num2 = 2
+num3 = num1 / num2
+print (type(num3))
---
+++ 2.2-tipos_de_dados/2_2atv4.py
@@ -0,0 +1,9 @@
+saldo = 500
+juros = 1.01
+print(saldo)
+saldo = saldo * juros
+print(saldo)
+saldo = saldo * juros
+print(saldo)
+saldo = saldo * juros
+print(saldo)
